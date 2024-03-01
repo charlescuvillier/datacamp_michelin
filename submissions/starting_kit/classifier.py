0a6abaed2a2e04236341dcfd222e8bc752a83a5f
Quentin36250 <82038152+Quentin36250@users.noreply.github.com>
2024-03-01 19:08:03 +0100

    Add files via upload

--- submissions/starting_kit/classifier.py
+++ submissions/starting_kit/classifier.py
@@ -1,29 +1,47 @@
 from sklearn.base import BaseEstimator
 from sklearn.pipeline import make_pipeline, Pipeline
 from sklearn.impute import SimpleImputer
-from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import StandardScaler,OrdinalEncoder
+from sklearn.compose import ColumnTransformer
 from sklearn.linear_model import LogisticRegression
 
 
 class Classifier(BaseEstimator):
     def __init__(self):
-        self.transformer = Pipeline(
+        text_column=['blurb']
+        cat_column=['name','city','country','chef','cuisine1','cuisine2']
+        num_column=['lat','lon']
+        #Preprocessing for numeric data
+        numeric_transformer = Pipeline(
+        steps = [
+        ("imputer", SimpleImputer(strategy="median")),  
+        ("scaler", StandardScaler())  
+        ])
+        #Preprocessing for categorical data
+        cat_transformer=Pipeline(
             steps=[
-                ("imputer", SimpleImputer(strategy="median")),
-                ("scaler", StandardScaler()),
-            ]
+                ("imputer2", SimpleImputer(strategy="most_frequent")),
+                ("encoder",OrdinalEncoder(handle_unknown="use_encoded_value",unknown_value=-1))
+                ]
         )
+
+
+        self.preprocessor = ColumnTransformer(
+            transformers=[
+                ("num", numeric_transformer, num_column),
+                ("cat", cat_transformer, cat_column),
+            ],remainder='drop' # Here not use the description of the restaurant
+        )
+
+        #The first model to beat use a logistic linear regressor
         self.model = LogisticRegression(max_iter=500)
-        self.pipe = make_pipeline(self.transformer, self.model)
+        self.pipe = make_pipeline(self.preprocessor, self.model)
 
     def fit(self, X, y):
-        X = X.drop(["groups"], axis=1)
         self.pipe.fit(X, y)
 
     def predict(self, X):
-        X = X.drop(["groups"], axis=1)
         return self.pipe.predict(X)
 
     def predict_proba(self, X):
-        X = X.drop(["groups"], axis=1)
         return self.pipe.predict_proba(X)
